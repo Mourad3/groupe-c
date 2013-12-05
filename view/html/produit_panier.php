ba77619054bcac958f58f9a201b906edc41bb176
younes1benkirane <younes1benkirane@gmail.com>
2013-12-05 23:35:22 +0000

    Signed-off-by: younes1benkirane <younes1benkirane@gmail.com>

--- view/html/produit_panier.php
+++ view/html/produit_panier.php
@@ -1,45 +1,43 @@
 <?php
-include 'includes/header.inc';
+$title='Validation du Panier';
+
+if (isset($_SESSION['login']))
+include 'view/html/includes/header.inc';
+else 
+include 'view/html/includes/headerlog.inc';
 ?>
 <div class="container">
 <br/>
-<h2>Validation Panier</h2>
-<form action="" method="" >
-	<table border="1">
+<h2>Validation Du Panier</h2>
+<form action="" method="post">
+	<table border="1" width="1000">
 		<tr>
-		<th>Id</th>
-		<th>Libelle</th>
+		<th>Identifiant</th>
+		<th>Produit</th>
+		<th>Catégorie</th>
+		<th>Couleur</th>
 		<th>Prix</th>
-		<th>description</th>
-		<th>Categorie</th>
-		<th>Validation d'achat</th>
+		<th>description</th>		
+		<th><input type="checkbox" name="v" onClick="checkAll('cases[]', this.checked);" /></th>
 		</tr>
 		<?php foreach($items as $item){?>
 		<tr>
-		<td><?php echo $item->getId();?></td>
-		<td><?php echo $item->getLibelle();?></td>
-		<td><?php echo $item->getPrix();?></td>
-		<td><?php echo $item->getDescription();?></td>
-		<td><?php echo $item->getCategorie();?></td>
-		<td><input type ="checkbox" name="v[<?php echo $item->getId();?>]" value="<?php echo $item->getId();?>"/></td>
-		</tr>
-		<tr>
-		<td>Nbre d'achats:</td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td>Cocher Tout<input type="checkbox" name="v6" id="tous" value="f" onclick="cocher(true);" /></td>
+		<td align="center"><?php echo $item->getId();?></td>
+		<td align="center"><?php echo $item->getLibelle();?></td>
+		<td align="center"><?php echo $item->getCategorie();?></td>
+		<td align="center"><?php echo $item->getCouleur();?></td>
+		<td align="center"><?php echo $item->getPrix();?> DH</td>
+		<td align="center"><?php echo $item->getDescription();?></td>		
+		<td align="center"><input type ="checkbox" name="cases[]" value="<?php echo $item->getId();?>"/></td>
 		</tr>
 		<?php } ?>
-	</table>
+	</table><br/>
+		<b>Nombre d'achats: <?php echo $produit->NombreProduitAchetes();?></b><br/>
+		<b>Prix Total: <?php echo $produit->PrixTotalProduit();?> dh</b>
 	<br/>
-	<input type="button" value="Validation d'achat"/><br/>
-	
+	<center><input type="submit" value="Validation du Panier" onClick="javascript:document.getElementById('validation').value='valid';"/></center><br/>
+	<input type="hidden" id="validation" name="validation" value=""/>
 </form>
 </center>			
 </div>
-
-<?php
-include 'includes/footer.inc';
-?>
+<?php include 'view/html/includes/footer.inc'; ?>
--- view/js/select.js
+++ view/js/select.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-$("#div1").hide();
-$("#div2").hide();
-$("#chart_div").hide;
-});
-function GereChkbox(conteneur, a_faire) {
-var blnEtat=null;
-var Chckbox = document.getElementById(conteneur).firstChild;
-	while (Chckbox!=null) {
-		if (Chckbox.nodeName=="INPUT")
-			if (Chckbox.getAttribute("type")=="checkbox") {
-				blnEtat = (a_faire=='0') ? false : (a_faire=='1') ? true : (document.getElementById(Chckbox.getAttribute("id")).checked) ? false : true;
-				document.getElementById(Chckbox.getAttribute("id")).checked=blnEtat;
-			}
-		Chckbox = Chckbox.nextSibling;
-	}
-}
 function checkAll(name, checked){
     //On parcourt tous les inputs de la page
     var inputs = document.getElementsByTagName('input');
@@ -28,25 +11,3 @@ function checkAll(name, checked){
     }
 	
 } 
-function check()
-{
-      if ($("#chek1").is(":checked")) {
-        $("#div1").show();
-		$("#div2").hide();
-    }
-	if ($("#chek2").is(":checked")) {
-        $("#div1").show();
-		$("#div2").show();
-    }
-}    
-
-function cocherTout(name, etat)
-{
-  var cases = document.getElementsByTagName('input');   // on recupere tous les INPUT
-   for(var i=1; i<cases.length; i++)     // on les parcourt
-     if(cases[i].type == 'checkbox' && inputs[i].name == name)     // si on a une checkbox...
-         {cases[i].checked = etat;}
-             // ... on la coche ou non
- 
- 
-}
