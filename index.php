623968e595c5db99e9e3ab9c88f340b1e1709808
reda48fethi <reda.fethi@gmail.com>
2013-12-05 00:07:32 +0000

    Signed-off-by: reda48fethi <reda.fethi@gmail.com>

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?php 
-
-// ceci est le controleur frontal :
-// son rôle est de rediriger  les requettes de l'internaute vers les différents controleurs de notre application
+<?php
 if(isset($_GET['c']))
 $c = $_GET['c'];
 else 
 $c='accueil';
-
-include('Controleur/' . $c .'.php');
-
+include('controller/' . $c .'.php');
 ?>
