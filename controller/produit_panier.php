a27c3148e75477cd476953ce54bc04189c2567cb
reda48fethi <reda.fethi@gmail.com>
2013-12-05 23:28:08 +0000

    Signed-off-by: reda48fethi <reda.fethi@gmail.com>

--- controller/produit_panier.php
+++ controller/produit_panier.php
@@ -1,7 +1,11 @@
 <?php
-include('../lib/db.php');
-include('../model/produit_panier.php');
+include('lib/db.php');
+include('model/produit_panier.php');
 $produit=new Produit_Panier();
+if(isset($_POST['validation'])){
+$cases=$_POST['cases'];
+$produit->ValidationPanier($cases);
+}
 $items=$produit->listeProduit();
-include('../view/html/produit_panier.php');
+include('view/html/produit_panier.php');
 ?>
