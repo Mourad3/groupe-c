b5a6c733dbc7a0e391c3b68c1205fe28b6f3d4b3
zakariaH <zakaria0390@gmail.com>
2013-12-07 21:34:07 +0000

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,9 +1,22 @@
 <?php
 class db{
+
 	
-	static public function connecte(){
-                mysql_connect(HOST_DB, USER_DB, PASS_DB) or die("erreur de connexion avec le serveur web");
-                mysql_select_db(NAME_DB)or die ("erreur de connexion avec la base de donnée");
+	public function __construct(){
+		mysql_connect('localhost', 'root', '')or die("erreur de connexion avec le serveur web");
+		mysql_select_db('ecommerce')or die ("erreur de connexion avec la base de donnée");
 	}
+	
+	public function executeQuery( $sql ){
+		
+		return mysql_query($sql)or die ('Ereur sql!'.$sql.'<br/>'.mysql_error());
+	
+	}
+	
+	public function __destruct(){
+		mysql_close();
+	}
+	
+	
 }
 ?>
